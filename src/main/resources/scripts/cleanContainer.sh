f4ab0334a3465211ef09259ad0fcd602cdea1320
Mitko Rachev <Dimitar.Iv.Rachev@gmail.com>
2024-04-16 23:54:02 +0300

    added scripts

---
+++ src/main/resources/scripts/cleanContainer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if container name is provided as argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <port> <container_name>"
+    exit 1
+fi
+
+# Assign arguments to variable
+container_name=$1
+
+# Check if the container exists
+if [[ "$(docker ps -q -f name=$container_name)" ]]; then
+    # Stop the container if it's running
+    docker stop $container_name >/dev/null 2>&1
+
+    # Remove the container
+    docker rm $container_name >/dev/null 2>&1
+
+    echo "Container stopped and removed successfully."
+elif [[ "$(docker ps -aq -f name=$container_name)" ]]; then
+    # Remove the container if it's not running but exists
+    docker rm $container_name >/dev/null 2>&1
+
+    echo "Container removed successfully."
+else
+    echo "Container  not found or not running."
+fi
---
+++ src/main/resources/scripts/createAndStart.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if port and container name are provided as arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <port> <container_name>"
+    exit 1
+fi
+
+# Assign arguments to variables
+port=$1
+container_name=$2
+
+# Generate Dockerfile content
+dockerfile_content="FROM openjdk:21-ea-30-slim-bookworm\n\nCOPY build/libs/eureka-*-SNAPSHOT.jar app.jar\n\nENTRYPOINT [\"java\", \"-jar\", \"/app.jar\"]"
+
+# Write Dockerfile
+echo -e "$dockerfile_content" > Dockerfile
+
+# Generate docker-compose.yml content
+docker_compose_content="version: '3.8'\n\nservices:\n  $container_name:\n    container_name: $container_name\n    build:\n      context: .\n      dockerfile: Dockerfile\n    environment:\n      - port=$port\n    ports:\n      - \"$port:$port\""
+
+# Write docker-compose.yml
+echo -e "$docker_compose_content" > docker-compose.yml
+
+# Start container using docker-compose
+docker compose up -d
+
+# Delete docker-compose.yml
+rm docker-compose.yml
+rm Dockerfile
+
+echo "Container $container_name started with port $port and docker-compose file deleted."
